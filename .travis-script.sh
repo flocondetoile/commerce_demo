5e00718e63b08284b1fb81928f56cb677259e670
Matt Glaman <nmd.matt@gmail.com>
2017-02-23 23:24:40 -0600

    more

--- .travis-script.sh
+++ .travis-script.sh
@@ -6,5 +6,3 @@ cd "$DRUPAL_TI_DRUPAL_DIR"
 drush cc drush
 drush mi --all
 
-echo "$MODULE_DIR/$DRUPAL_TI_MODULE_NAME/$DRUPAL_TI_PHPUNIT_CORE_SRC_DIRECTORY"
-echo $(cd "$DRUPAL_TI_MODULES_PATH"; pwd)
